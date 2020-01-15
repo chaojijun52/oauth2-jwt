47621167bb39c78ce86a5e877edef21a8c758ada
Wu Jun <409994884@qq.com>
2020-01-15 22:13:59 +0800

    ClassPathResource+RestTemplate

--- Oauth2Client/src/main/java/com/example/Oauth2Client/controller/ServiceController.java
+++ Oauth2Client/src/main/java/com/example/Oauth2Client/controller/ServiceController.java
@@ -2,25 +2,56 @@ package com.example.Oauth2Client.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestOperations;
+import org.springframework.web.client.RestTemplate;
+
+import com.example.Oauth2Client.entity.LoginRequest;
+import com.example.Oauth2Client.entity.OauthEntity;
 
 @RestController
 @Configuration
 public class ServiceController {
 
-@Autowired
-public RestOperations template;
-	
-    @RequestMapping(value="/test",method=RequestMethod.POST, produces={"application/JSON"}, consumes={"text/plain"})
-    public String getShipmentDetails(@RequestParam(value="name", defaultValue="0") String name) {
-        ResponseEntity<String> result = template.postForEntity("http://localhost:9080/hello",name,String.class);
-        return result.getBody();
-    }
-    
+	@Autowired
+	public RestOperations template;
+	private RestTemplate restTemplate = new RestTemplate();
+
+	@RequestMapping(value = "/test", method = RequestMethod.POST, produces = { "application/JSON" }, consumes = {
+			"text/plain" })
+	public String getShipmentDetails(@RequestParam(value = "name", defaultValue = "0") String name) {
+		ResponseEntity<String> result = template.postForEntity("http://localhost:9080/hello", name, String.class);
+		return result.getBody();
+	}
+
+	@PostMapping(path = "/login/verify")
+	public @ResponseBody OauthEntity login(@RequestBody LoginRequest loginRequest) {
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+		headers.setBasicAuth(loginRequest.getClientId(), loginRequest.getClientSecret());// ("testjwtclientid",
+																							// "jwtpass");
+		MultiValueMap<String, Object> map = new LinkedMultiValueMap<String, Object>();
+		map.add("grant_type", loginRequest.getGrantType());// "password");
+		map.add("username", loginRequest.getUsername());// "admin");
+		map.add("password", loginRequest.getPassword());// "jwtpass");
+		HttpEntity<MultiValueMap<String, Object>> request = new HttpEntity<MultiValueMap<String, Object>>(map, headers);
+		ResponseEntity<OauthEntity> result = restTemplate.postForEntity("http://localhost:9090/oauth/token", request,
+				OauthEntity.class);
+		System.out.println("Status code: " + result.getStatusCodeValue());
+		return result.getBody();
+	}
 
 }
--- Oauth2ResourceServer/src/main/java/com/example/Oauth2ResourceServer/config/SecurityConfig.java
+++ Oauth2ResourceServer/src/main/java/com/example/Oauth2ResourceServer/config/SecurityConfig.java
@@ -1,6 +1,10 @@
 package com.example.Oauth2ResourceServer.config;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -30,76 +34,71 @@ public class SecurityConfig extends ResourceServerConfigurerAdapter {
 //	@Value("${security.signing-key}")
 //    private String signingKey;
 
-    @Value("${security.encoding-strength}")
-    private Integer encodingStrength;
+	@Value("${security.encoding-strength}")
+	private Integer encodingStrength;
 
-    @Value("${security.security-realm}")
-    private String securityRealm;
-    
+	@Value("${security.security-realm}")
+	private String securityRealm;
+
 //	@Autowired
 //    private ResourceServerTokenServices tokenServices;
 //
 //    @Autowired
 //    private JwtAccessTokenConverter accessTokenConverter;
 //    
-    @Override
-    @Order(Ordered.HIGHEST_PRECEDENCE)
+	@Override
+	@Order(Ordered.HIGHEST_PRECEDENCE)
 	public void configure(HttpSecurity http) throws Exception {
-		http
-		.sessionManagement()
-		.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-		.and()
-		.csrf().disable()
-	  	.authorizeRequests()
-        .anyRequest().authenticated()
-	  	.and()
-	  	.httpBasic()
-	  		.realmName("CRM_REALM");
-    }
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().csrf().disable()
+				.authorizeRequests().anyRequest().authenticated().and().httpBasic().realmName("CRM_REALM");
+	}
 
+	@Override
+	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+		// do not require a resource id in AccessToken.
+		resources.resourceId(null);
+		resources.tokenServices(tokenServices());
+	}
 
-    @Override
-    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
-        // do not require a resource id in AccessToken.
-        resources.resourceId(null);
-        resources.tokenServices(tokenServices());
-    }
-    
-    @Bean
-    public TokenStore tokenStore() {
-        return new JwtTokenStore(accessTokenConverter());
-    }
- 
-    @Bean
-    public JwtAccessTokenConverter accessTokenConverter() {
-        JwtAccessTokenConverter converter = new CustomJwtAccessTokenConverter();//new JwtAccessTokenConverter();
-        Resource resource=new ClassPathResource("public.txt");
-        String publicKey = null;
-        try {
-            publicKey = resource.getInputStream().toString();
-        } catch (final IOException e) {
-            throw new RuntimeException(e);
-        }
+	@Bean
+	public TokenStore tokenStore() {
+		return new JwtTokenStore(accessTokenConverter());
+	}
+
+	@Bean
+	public JwtAccessTokenConverter accessTokenConverter() {
+		JwtAccessTokenConverter converter = new CustomJwtAccessTokenConverter();// new JwtAccessTokenConverter();
+		Resource resource = new ClassPathResource("public.txt");
+		String publicKey = null;
+		try {
+			InputStream inputStream=resource.getInputStream();
+			BufferedReader reader=new BufferedReader(new InputStreamReader(inputStream));
+			publicKey = reader.lines().collect(Collectors.joining());//resource.getInputStream().toString();
+		} catch (final IOException e) {
+			throw new RuntimeException(e);
+		}
 //        converter.setSigningKey(signingKey);
-        System.out.println("Public key: "+publicKey);
-        publicKey="-----BEGIN PUBLIC KEY-----\r\n" + 
-        		"MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAlf/N+8VkgpZt7p+SgkMA\r\n" + 
-        		"w7XDuB83AW9yeuqrDEbc+eai6L9AYAuxdYLRwLSMAzl5x9lDc0vNkEfGSuv8j3z/\r\n" + 
-        		"b4ZcppnnmO9MS+QFUfEAR8/RtyCdAHJRwua9rAkqFeWr1zPyj2o56Fz83/4ivW4E\r\n" + 
-        		"fAwXScVxaXBc1ccvTTKaeJcTa56b/8XqOFUNZKiI1ovOvm1J4avs+9zviUo1DD9E\r\n" + 
-        		"D9ao9aOMCvs5OsH28M26FJqUgJaF4sTNf0so0kB944mhVr9HnAAs4VJoJgJsJTkR\r\n" + 
-        		"jBl7Y2Jr7IrNJ7meTOSsQOFlSEAciiej2DtZxtyT395BN5L2FeJE4oDal+dnY17P\r\n" + 
-        		"pwIDAQAB\r\n" + 
-        		"-----END PUBLIC KEY-----";
-        converter.setVerifierKey(publicKey);
-        return converter;
-    }
- 
-    @Bean
-    @Primary
-    public DefaultTokenServices tokenServices() {
-        DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
-        defaultTokenServices.setTokenStore(tokenStore());
-        return defaultTokenServices;
-    }
+		System.out.println(
+				"\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n===============================Public key========================================                "
+						+ publicKey+"\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n");
+//		publicKey = "-----BEGIN PUBLIC KEY-----"
+//				+ "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAlf/N+8VkgpZt7p+SgkMA"
+//				+ "w7XDuB83AW9yeuqrDEbc+eai6L9AYAuxdYLRwLSMAzl5x9lDc0vNkEfGSuv8j3z/"
+//				+ "b4ZcppnnmO9MS+QFUfEAR8/RtyCdAHJRwua9rAkqFeWr1zPyj2o56Fz83/4ivW4E"
+//				+ "fAwXScVxaXBc1ccvTTKaeJcTa56b/8XqOFUNZKiI1ovOvm1J4avs+9zviUo1DD9E"
+//				+ "D9ao9aOMCvs5OsH28M26FJqUgJaF4sTNf0so0kB944mhVr9HnAAs4VJoJgJsJTkR"
+//				+ "jBl7Y2Jr7IrNJ7meTOSsQOFlSEAciiej2DtZxtyT395BN5L2FeJE4oDal+dnY17P"
+//				+ "pwIDAQAB"
+//				+ "-----END PUBLIC KEY-----";
+		converter.setVerifierKey(publicKey);
+		return converter;
+	}
+
+	@Bean
+	@Primary
+	public DefaultTokenServices tokenServices() {
+		DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
+		defaultTokenServices.setTokenStore(tokenStore());
+		return defaultTokenServices;
+	}
 }
